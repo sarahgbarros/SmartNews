3d7e562e2a58238544bbe464be0d417a7f4e15fd
Sarah Barros <sarahgbarros@hotmail.com>
2025-09-26 02:19:03 -0400

    feat: Django Admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'date_joined')
+    search_fields = ('username', 'email')
+    list_filter = ('date_joined',)
+
+
+admin.site.register(User, UserAdmin)
